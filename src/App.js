7697264c71455bdaaf0e54e37c85ce247b052cd4
IT20122096 <it20122096@my.sliit.lk>
2023-10-05 17:53:05 +0530

    IT20122096- log reg front end screens

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import AppHeader from "./components/common/Header";
+
+import LoginRegPage from "./components/userManagement/LoginRegPage";
+
+import Footer from "./components/common/Footer";
+
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-header"></div>
-        <div className="app-body">
+        <div className="app-header">
+          <AppHeader />
+        </div>
+        <div
+          className="app-body"
+          style={{ height: "79.2vh", margin: "1rem", border: "1px solid black" }}
+        >
           <Switch>
-            <div>Hello world</div>
+            <Route path="/" component={LoginRegPage} />
           </Switch>
         </div>
-        <div className="app-footer"></div>
+        <div className="app-footer">
+          <Footer />
+        </div>
       </div>
     );
   }
 }
-
---
+++ src/components/userManagement/TravelAgentLogForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TravelAgentLogForm extends Component {
+  render() {
+    return (
+      <div>TravelAgentLogForm</div>
+    )
+  }
+}
